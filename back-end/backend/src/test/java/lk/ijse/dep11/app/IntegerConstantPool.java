e3baff55cabac11d319e7b98780332c96b4c621c
isuri <isuriwgunaratne@gmail.com>
2023-12-21 14:20:31 +0530

    Add pnpm, parcel, bootstrap, and bootstrap-icons as development dependencies

---
+++ back-end/backend/src/test/java/lk/ijse/dep11/app/IntegerConstantPool.java
@@ -0,0 +1,14 @@
+//package lk.ijse.dep11.app;
+///**
+// * A class to demonstrate the behavior of Integer constant pool.
+// */
+//public class IntegerConstantPool {
+//
+//    public static void main(String[] args) {
+//        Integer i = 50;
+//        Integer i2 = 50;
+//        System.out.println(i == i2);
+//        System.out.println(i.equals(i2));
+//
+//    }
+//}
---
+++ back-end/backend/src/test/java/lk/ijse/dep11/app/JVMEnv.java
@@ -0,0 +1,28 @@
+//package lk.ijse.dep11.app;
+//
+//import java.util.Properties;
+//import java.util.Set;
+//
+///**
+// * A class to demonstrate how to access and display JVM system properties.
+// */
+//public class JVMEnv {
+//
+//    /**
+//     * The main method of the program.
+//     *
+//     * @param args Command-line arguments (not used in this example).
+//     */
+//    public static void main(String[] args) {
+//        // Get the system properties
+//        Properties properties = System.getProperties();
+//
+//        // Get the set of property names
+//        Set<Object> names = properties.keySet();
+//
+//        // Iterate through each property and print its name and value
+//        for (Object name : names) {
+//            System.out.printf("%s: %s \n", name, properties.get(name));
+//        }
+//    }
+//}
